93b129d5e0becdbdb09b313a9a4f59e1cceffe01
Aniket Mahato <aniket.mahata.06@gmail.com>
2023-12-07 21:38:36 +0530

    Angular practice mat-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import {
+  MatAutocomplete,
+  // MatAutocomplete,
+  MatAutocompleteModule,
+  MatAutocompleteTrigger,
+  // MatAutocompleteTrigger,
+} from '@angular/material/autocomplete';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-mat-auto-complete';
+export class AppComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatAutocomplete) autocomplete: MatAutocomplete | any;
+  options: string[] = ['One', 'Two', 'Three'];
+  dummy: boolean = false;
+  // formControl = new FormControl('');
+  metaData: FormGroup = new FormGroup({
+    location: new FormControl(''),
+  });
+  ngOnInit(): void {
+    console.log(this.metaData.value);
+  }
+  ngAfterViewInit(): void {
+    this.metaData.controls['location'].valueChanges.subscribe((res) => {
+      console.log('Location value:', res);
+    });
+  }
+  onClick() {
+  console.log('clicked');
+    this.dummy = !this.dummy;
+    this.metaData.controls['location'].patchValue(null);
+    console.log(this.autocomplete.options);
+  }
+
+  formatLabel(value: any) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+
+  check(event: any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
